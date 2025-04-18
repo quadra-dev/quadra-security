a86f172a3c4427619754dc2aac9ca2a5c9cc6fd4
Semicile17 <Rohitsinghmahant707@gmail.com>
2025-04-18 18:21:38 +0530

    created feature home

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript",{
+    rules: {
+      // Note: you must disable the base rule as it can report incorrect errors
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error"
+    }
+  }),
+  
 ];
 
 export default eslintConfig;
