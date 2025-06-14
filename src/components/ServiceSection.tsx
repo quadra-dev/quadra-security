14d03748076277b529f6265517c54109aa72cde4
quadra-dev <quadradev04@gmail.com>
2025-06-14 23:42:12 +0530

    Update ServiceSection.tsx

--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -61,7 +61,7 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
     >
       {/* Icon and line */}
       <div className="flex md:flex-col items-center md:items-start md:mr-5 relative z-10 w-full md:w-auto">
-        <div className="p-2 text-md flex items-center justify-center text-3xl bg-blue-800 text-black rounded-full relative z-20">
+        <div className="p-2 text-md flex items-center justify-center text-3xl bg-blue-800 text-white rounded-full relative z-20">
           {icon}
         </div>
         {/* Line positioned differently on mobile and desktop */}
@@ -80,7 +80,7 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
 
       {/* Text content */}
       <div className={`flex-1 max-w-full md:max-w-[55%]`}>
-        <h2 className="text-xl font-bold text-white mb-3 relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-10 after:h-0.5 after:bg-[#f0b100]">
+        <h2 className="text-xl font-bold text-blue-800 mb-3 relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-10 after:h-0.5 after:bg-[#f0b100]">
           {title}
         </h2>
         <p className="text-gray-800 mb-4 leading-relaxed whitespace-pre-line">{description}</p>
